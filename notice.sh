4ee0ea45f1484b03b9858c1b37d67fc9cb35839b
Kat Traxler <kat@nanooksecurity.com>
2021-05-23 17:16:42 -0500

    initial commit

---
+++ notice.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+if [ "$(shuf -i 1-20 -n 1)" -eq 1 ]; then
+  echo '
+                   ***** NOTICE *****
+Supported `curl` images, including multiple tagged versions,
+are available at
+https://console.cloud.google.com/launcher/details/google/ubuntu1604
+and https://hub.docker.com/r/curlimages/curl.
+                ***** END OF NOTICE *****
+'
+fi
+curl "$@"
